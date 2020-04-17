5d918559077b8b8b55c26eacd1aec8f5e2e0b2fd
elisa-rodrigues <62354637+elisa-rodrigues@users.noreply.github.com>
2020-04-17 20:09:11 -0300

    Update Ex9.c

--- Listas/Lista2/Ex9/Ex9.c
+++ Listas/Lista2/Ex9/Ex9.c
@@ -9,7 +9,52 @@ Toda vez que João vai calcular as raízes de uma equação de 2o grau, esquece 
     (d) Quais casos é preciso verificar?
 */
 
-void main()
-{
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
+int main() 
+{
+  float a, b, c;
+  float delta;
+  float x1, x2;
+  
+  printf("Digite os coeficientes a, b e c da equação do segundo grau.\n");
+  scanf("%f%f%f", &a, &b, &c);
+  
+  printf("\nEquação: %.2f (x2) + %.2f (x) + %.2f\n\n", a, b, c);
+  
+  if(a == 0)
+  {
+    printf("Não é uma equação do segundo grau.\n\n");
+  }
+  
+  else
+  {
+    delta = pow(b,2) - (4.0 * a * c);
+    printf("Delta: %.2f \n\n", delta);  
+    
+    if(delta < 0)
+    {
+      printf("Não existem raízes.\n\n");  
+    
+    }     
+    else if(delta == 0)
+    {
+      x1 = (-b + sqrt(delta)) / (2*a);
+      
+      printf("Existe apenas uma raiz. \n");  
+      printf(" x = %.2f \n", x1);
+      
+    }     
+    else
+    {
+      x1 = (-b + sqrt(delta)) / (2*a);
+      x2 = (-b - sqrt(delta)) / (2*a);
+      printf("Existem duas raízes: \n");  
+      printf(" x1 = %.2f \n x2 = %.2f \n", x1, x2);
+    }
+  }
+  
+  return 0;
 }
