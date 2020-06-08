bd49fb9355d0164137392660ae6abfb16661c2ec
Elisa Rodrigues <62354637+elisa-rodrigues@users.noreply.github.com>
2020-06-08 16:26:39 -0300

    Create Ex5.c

---
+++ Listas/Lista6/Ex5/Ex5.c
@@ -0,0 +1,12 @@
+/*
+O código de César é uma das técnicas de criptografia mais simples e conhecidas. É um tipo de substituição no 
+qual cada letra do texto é substituída por outra, que se apresenta n posições após ela no alfabeto: 
+a b c d e f g h i j k l m n o p q r s t u v w x y z . 
+(Por exemplo, com uma troca de três posições, a letra a seria substituída por d , b se tornaria e e assim por diante.) Escreva um programa que faça uso
+desse código de César para n posições. Entre com n , um vetor de caracteres (string) e imprima a string codificada. Exemplo:
+n: 2
+String: universidade
+Nova string: wpkxgtukfcfg
+
+(Ex. 14 - pag. 143 [BACKES, André. Linguagem C: completa e descomplicada. 2013.])
+*/
