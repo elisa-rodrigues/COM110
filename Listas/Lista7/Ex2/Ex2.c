0278d02f139e2bac9e54cb9cbc4a5dca7dd7e593
Elisa Rodrigues <62354637+elisa-rodrigues@users.noreply.github.com>
2020-06-08 16:30:58 -0300

    Create Ex2.c

---
+++ Listas/Lista7/Ex2/Ex2.c
@@ -0,0 +1,6 @@
+/*
+Crie um programa que preencha uma matriz 8 × 8 com números inteiros e mostre uma mensagem dizendo se a 
+matriz digitada é simétrica. Uma matriz só pode ser considerada simétrica se A[i, j] = A[j, i].
+
+(Ex. 11 - pag 230 [ASCÊNCIO, Ana Fernanda G.; CAMPOS, Edilene A. V. Fundamentos da Programação de Computadores. 2012.])
+*/
