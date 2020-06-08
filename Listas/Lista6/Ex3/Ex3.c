d31b8426f6c75a53ae8def03b5fbda5dadef41cc
Elisa Rodrigues <62354637+elisa-rodrigues@users.noreply.github.com>
2020-06-08 16:24:25 -0300

    Create Ex3.c

---
+++ Listas/Lista6/Ex3/Ex3.c
@@ -0,0 +1,6 @@
+/*
+Faça um programa que receba o número sorteado por um dado em vinte jogadas. Mostre os números sorteados e a 
+frequência com que aparecem.
+
+(Ex. 18 - pag. 180 [ASCÊNCIO, Ana Fernanda G.; CAMPOS, Edilene A. V. Fundamentos da Programação de Computadores. 2012.])
+*/
