fcf34fb8a0a301bcec37f22fb1d836bbfade572e
Elisa Rodrigues <62354637+elisa-rodrigues@users.noreply.github.com>
2020-06-08 16:32:45 -0300

    Create Ex4.c

---
+++ Listas/Lista7/Ex4/Ex4.c
@@ -0,0 +1,8 @@
+/*
+Um elemento A ij de uma matriz é dito ponto de sela da matriz A se, e somente
+se, A ij for, ao mesmo tempo, o menor elemento da linha i e o maior elemento da coluna j . Faça um
+programa que carregue uma matriz de ordem 5×7 , verique se a matriz possui ponto de sela e, se possuir,
+mostre seu valor e sua localização.
+
+(Ex. 7 - pag 225 [ASCÊNCIO, Ana Fernanda G.; CAMPOS, Edilene A. V. Fundamentos da Programação de Computadores. 2012.]) 
+*/
