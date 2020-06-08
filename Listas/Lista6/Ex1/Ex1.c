04ea22299fa490a056fce09646d78a4d4f102d7a
Elisa Rodrigues <62354637+elisa-rodrigues@users.noreply.github.com>
2020-06-08 16:20:25 -0300

    Create Ex1.c

---
+++ Listas/Lista6/Ex1/Ex1.c
@@ -0,0 +1,6 @@
+/*
+Faça um programa que, no momento de preencher um vetor com oito números inteiros, já os armazene de forma crescente.
+(Ex. 7 - pag. 166 (ASCÊNCIO, Ana Fernanda G.; CAMPOS, Edilene A. V. Fundamentos da Programação de Computadores. 2012.)) 
+*/
+
+
