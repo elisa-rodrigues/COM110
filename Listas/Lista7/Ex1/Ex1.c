34028a69bedc65d27035adf741270efdc3497b9a
Elisa Rodrigues <62354637+elisa-rodrigues@users.noreply.github.com>
2020-06-08 16:29:36 -0300

    Create Ex1.c

---
+++ Listas/Lista7/Ex1/Ex1.c
@@ -0,0 +1,6 @@
+/*
+Faça um programa que preencha uma matriz M (2 × 2) com números inteiros, calcule e mostre a matriz R, 
+resultante da multiplicação dos elementos de M pelo seu maior elemento.
+
+(Ex. 1 - pag 220 [ASCÊNCIO, Ana Fernanda G.; CAMPOS, Edilene A. V. Fundamentos da Programação de Computadores. 2012.]) 
+*/
