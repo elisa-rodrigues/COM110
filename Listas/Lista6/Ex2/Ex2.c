5f29df6712274c32153826fb306b5275a1b6c4fc
Elisa Rodrigues <62354637+elisa-rodrigues@users.noreply.github.com>
2020-06-08 16:22:27 -0300

    Create Ex2.c

---
+++ Listas/Lista6/Ex2/Ex2.c
@@ -0,0 +1,9 @@
+/*
+Faça um programa para corrigir provas de múltipla escolha. Cada prova tem oito questões e cada questão vale um ponto. 
+O primeiro conjunto de dados a ser lido é o gabarito da prova. Os outros dados são os números dos alunos e as respostas 
+que deram às questões. Existem dez alunos matriculados. Calcule e mostre:
+(a) o número e a nota de cada aluno;
+(b) a porcentagem de aprovação, sabendo-se que a nota mínima é 6.
+
+(Ex. 10 - pag. 171 [ASCÊNCIO, Ana Fernanda G.; CAMPOS, Edilene A. V. Fundamentos da Programação de Computadores. 2012.])
+*/
