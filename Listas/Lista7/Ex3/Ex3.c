135c00fafedd55dc2a747741004b383ef76971a0
Elisa Rodrigues <62354637+elisa-rodrigues@users.noreply.github.com>
2020-06-08 16:31:57 -0300

    Create Ex3.c

---
+++ Listas/Lista7/Ex3/Ex3.c
@@ -0,0 +1,7 @@
+/*
+Na teoria dos sistemas, dene-se o elemento MINMAX de uma matriz como o maior elemento da linha em que se encontra 
+o menor elemento da matriz. Elabore um programa que carregue uma matriz 4 × 7 com números reais, calcule e mostre 
+seu MINMAX e sua posição (linha e coluna).
+
+(Ex. 5 - pag 223 [ASCÊNCIO, Ana Fernanda G.; CAMPOS, Edilene A. V. Fundamentos da Programação de Computadores. 2012.])
+*/
