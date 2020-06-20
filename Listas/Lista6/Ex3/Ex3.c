4f8d57b87b04093f0a467542780d305fb96fba47
Elisa Rodrigues <62354637+elisa-rodrigues@users.noreply.github.com>
2020-06-20 13:13:17 -0300

    Update Ex3.c

--- Listas/Lista6/Ex3/Ex3.c
+++ Listas/Lista6/Ex3/Ex3.c
@@ -4,3 +4,35 @@ frequência com que aparecem.
 
 (Ex. 18 - pag. 180 [ASCÊNCIO, Ana Fernanda G.; CAMPOS, Edilene A. V. Fundamentos da Programação de Computadores. 2012.])
 */
+
+// Autor : Daniel Henrique Ferreira de Andrade
+// Junho de 2020
+
+#include <stdio.h>
+
+int main(){
+    int dado, freq[6]={0,0,0,0,0,0},i,k;//Todos os valores de frequencia devem começar no 0
+
+    for(i = 0;i < 20;i++)//Joga-se o dado 20 vezes
+    {
+        printf("Digite o valor tirado no dado: ");
+        scanf("%d",&dado);
+
+                    if(dado == 1)//Pra cada valor do dado, uma das posições da frequencia será incrementado
+                        freq[0]++;
+                    else if(dado == 2)
+                        freq[1]++;
+                    else if(dado == 3)
+                        freq[2]++;
+                    else if(dado == 4)
+                        freq[3]++;
+                    else if(dado == 5)
+                        freq[4]++;
+                    else
+                        freq[5]++;
+            
+    }
+
+    for(i = 0;i < 6;i++)//Imprime as frequencias
+        printf("O numero %d apareceu %d vezes\n",i+1,freq[i]);
+}
