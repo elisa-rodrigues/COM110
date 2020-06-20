12ad5d849bfd3263f6809b0bb6c5daf3552f43d3
Elisa Rodrigues <62354637+elisa-rodrigues@users.noreply.github.com>
2020-06-20 13:16:26 -0300

    Update Ex5.c

--- Listas/Lista6/Ex5/Ex5.c
+++ Listas/Lista6/Ex5/Ex5.c
@@ -10,3 +10,53 @@ Nova string: wpkxgtukfcfg
 
 (Ex. 14 - pag. 143 [BACKES, André. Linguagem C: completa e descomplicada. 2013.])
 */
+
+// Autor : Daniel Henrique Ferreira de Carvalho
+// Junho de 2020
+
+#include <stdio.h>
+#include <string.h>
+
+#define Tamanho 6 
+#define TamanhoA 26 
+
+int main(void) {
+
+  int Nivel, Temp,i,j,tam;
+  char Palavra[Tamanho], Criptografia[Tamanho];
+  char Alfabeto[TamanhoA] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+                            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'}; 
+
+  printf("Digite a palavra que gostaria de criptografar: ");
+  scanf("%s", Palavra);
+
+  printf("Digite o nível da criptografia: ");
+  scanf("%d", &Nivel);
+
+tam = strlen(Palavra);//Armazena o tamanho da string para caso o usuario digite menos de 6 caracteres
+
+  // armazena em Criptografia a nova palavra
+  for(i=0; i<tam; i++){
+
+    j=0; // zero o contador para a próxima iteração do laço
+
+    // verifica em qual posição do vetor Alfabeto está a letra da posição i da Palavra 
+    while(Palavra[i] != Alfabeto[j]){
+      j++;
+    }
+
+    // verifica se a nova letra ultrapassa o limite do vetor Alfabeto
+    if(j + Nivel < TamanhoA){
+      Criptografia[i] = Alfabeto[j + Nivel];
+    }else{
+      Criptografia[i] = Alfabeto[(j + Nivel)-TamanhoA];
+    }
+  }
+
+    Criptografia[i] = '\0';//Adiciona o \0 ao final do arquivo para reconhecer como string
+
+  printf("Palavra criptografada: %s", Criptografia);
+
+  return 0;
+
+}
